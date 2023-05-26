810f31d590b1e3d906859b1c8ace2c19e5f6a467
Maksim Semenov <maksimuma@list.ru>
2023-05-26 15:31:44 +0300

    feat: создать схемы и модели для ресурсов API

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        validator.isEmail(v);
+      },
+      message: 'Некорректный email',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlenght: 30,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
