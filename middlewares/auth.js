7e3ab02c0265c1e0b1075df92d78c28cd8b9e535
Maksim Semenov <maksimuma@list.ru>
2023-05-29 14:12:02 +0300

    refactor: добавить константы для ошибок

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,12 +2,13 @@ const jwt = require('jsonwebtoken');
 
 const { NODE_ENV, JWT_SECRET } = require('../config');
 const AuthError = require('../errors/AuthError');
+const { authErrorMessage } = require('../constants/constants');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new AuthError('Необходима авторизация'));
+    return next(new AuthError(authErrorMessage));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -16,7 +17,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return next(new AuthError('Необходима авторизация'));
+    return next(new AuthError(authErrorMessage));
   }
 
   req.user = payload;
