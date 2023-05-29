ad71020d6a00be2a85efadbbffb4724dd2500311
Maksim Semenov <maksimuma@list.ru>
2023-05-29 14:21:34 +0300

    feat: добавить helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { celebrate, Joi, errors } = require('celebrate');
+const helmet = require('helmet');
 
 const { PORT, DB } = require('./config');
 const router = require('./routes');
@@ -19,6 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(requestLogger);
+
+app.use(helmet());
 
 app.post('/signup', celebrate({
   body: Joi.object().keys({
