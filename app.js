1d508c861d767ffd46208046970be6d532ab59ff
Maksim Semenov <maksimuma@list.ru>
2023-05-29 15:53:37 +0300

    refactor: вынести роуты в index.js

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { celebrate, Joi, errors } = require('celebrate');
+const { errors } = require('celebrate');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
 const { PORT, DB } = require('./config');
 const router = require('./routes');
-const { createUser, login, logout } = require('./controllers/users');
-const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
 const NotFoundError = require('./errors/NotFoundError');
-const { EMAIL_REG, notFoundErrorMessage } = require('./constants/constants');
+const { notFoundErrorMessage } = require('./constants/constants');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const limiter = require('./middlewares/rateLimiter');
 
@@ -26,23 +24,6 @@ app.use(requestLogger);
 
 app.use(helmet());
 app.use(limiter);
-
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    email: Joi.string().required().pattern(EMAIL_REG),
-    password: Joi.string().required(),
-  }),
-}), createUser);
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().pattern(EMAIL_REG),
-    password: Joi.string().required(),
-  }),
-}), login);
-app.post('/signout', logout);
-
-app.use(auth);
 
 app.use('/', router);
 
