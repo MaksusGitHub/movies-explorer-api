1eee9c90c6432190ff49513cdb3bc0a5daa730c1
Maksim Semenov <maksimuma@list.ru>
2023-06-30 19:01:58 +0300

    hotfix: вернуть CORS

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-// const cors = require('cors');
+const cors = require('cors');
 
 const { PORT, DB } = require('./config');
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 const NotFoundError = require('./errors/NotFoundError');
-// const { NOT_FOUND_ERROR_MESSAGE, CORS_OPTIONS } = require('./constants/constants');
+const { NOT_FOUND_ERROR_MESSAGE, CORS_OPTIONS } = require('./constants/constants');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const limiter = require('./middlewares/rateLimiter');
 
@@ -17,36 +17,7 @@ const app = express();
 
 mongoose.connect(DB);
 
-const allowedCors = [
-  'https://maksus.movies-explorer.nomoredomains.rocks',
-  'http://maksus.movies-explorer.nomoredomains.rocks',
-  'http://localhost:3001',
-];
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  const { method } = req;
-
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-
-    return res.end();
-  }
-
-  return next();
-});
-
-// app.use(cors(CORS_OPTIONS));
+app.use(cors(CORS_OPTIONS));
 
 app.use(cookieParser());
 app.use(express.json());
