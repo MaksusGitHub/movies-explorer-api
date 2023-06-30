19e89fc17c6b12738ee9bb86954d0c5c6b6745cc
Maksim Semenov <maksimuma@list.ru>
2023-06-30 21:43:43 +0300

    hotfix: добавить origin

--- app.js
+++ app.js
@@ -24,18 +24,16 @@ const allowedCors = [
 ];
 
 app.use((req, res, next) => {
-  // const { origin } = req.headers;
+  const { origin } = req.headers;
 
-  // if (allowedCors.includes(origin)) {
-  //   res.header('Access-Control-Allow-Origin', origin);
-  // }
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
 
   const { method } = req;
 
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
-
-  res.header('Access-Control-Allow-Origin', '*');
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
