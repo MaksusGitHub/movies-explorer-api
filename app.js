eefb31ecc6e00bb27712a00173207c8d3e2fafcf
Maksim Semenov <maksimuma@list.ru>
2023-06-30 17:13:54 +0300

    feat: добавить CORS

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const { PORT, DB } = require('./config');
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 const NotFoundError = require('./errors/NotFoundError');
-const { NOT_FOUND_ERROR_MESSAGE } = require('./constants/constants');
+const { NOT_FOUND_ERROR_MESSAGE, CORS_OPTIONS } = require('./constants/constants');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const limiter = require('./middlewares/rateLimiter');
 
 const app = express();
 
 mongoose.connect(DB);
+
+app.use(cors(CORS_OPTIONS));
 
 app.use(cookieParser());
 app.use(express.json());
