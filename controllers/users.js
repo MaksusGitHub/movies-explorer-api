cd4a520f7d3ffb543916a1382a042b8b56bf810f
Maksim Semenov <maksimuma@list.ru>
2023-06-30 21:47:38 +0300

    hotfix: убрать same-site

--- controllers/users.js
+++ controllers/users.js
@@ -94,7 +94,7 @@ const login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000,
         httpOnly: true,
-        sameSite: true,
+        // sameSite: true,
       });
       res.send({ token, message: LOGIN_MESSAGE });
     })
